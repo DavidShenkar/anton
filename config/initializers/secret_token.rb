13c9075a075d629b5b3f13c450853779af440f2d
DavidShenkar <david.shenkar@gmail.com>
2013-12-23 17:12:09 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anton::Application.config.secret_key_base = '1703179f732b6b010423c0d29577cdc978d81eb7fa9abe983d0075a10e9d3efcfb14c5d1cfbfca841a5d8f26e8a4105a41924846091978c0a7aaa295b7deac38'
